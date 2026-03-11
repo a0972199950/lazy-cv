9a2d22f3f7fb7b2acd77e718e43b54f6a85482be
john.hsieh@bytedance.com <john.hsieh@bytedance.com>
2026-03-11 11:46:22 +0800

    fix static path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.IS_GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
+  // 只有在部署到 GitHub Pages 時才加上這個路徑前綴
+  basePath: isGithubPages ? '/lazy-cv' : '',
+  assetPrefix: isGithubPages ? '/lazy-cv/' : '',
+
   output: 'standalone',
   
   images: {
